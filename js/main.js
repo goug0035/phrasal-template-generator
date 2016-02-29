a548e7121413dfdfa2caafc446dbf6b76756e952
goug0035 <marygough@Marys-MacBook-Pro.local>
2016-02-28 23:42:28 -0500

    first

---
+++ js/main.js
@@ -0,0 +1,15 @@
+alert('Let’s make a story together!');
+
+var colour = prompt('A colour (orange, indigo, etc.)');
+var creature = prompt('A creature (dragon, moth, etc.)');
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+var verd = prompt('A past tense verb (ran, burped, etc.)');
+
+var a = ('a) Snacking')
+var b = ('b) Escaping')
+var c = ('c) Winning')
+
+for (i = 0; i < 10; i++) {
+  document.write(letter);
+  document.write('<br>');
+}
