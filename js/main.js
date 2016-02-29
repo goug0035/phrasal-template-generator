0a8902c17b49b6b71fc298cd51cae58343075647
goug0035 <marygough@Marys-MacBook-Pro.local>
2016-02-28 23:55:10 -0500

    story added

--- js/main.js
+++ js/main.js
@@ -5,11 +5,16 @@ var creature = prompt('A creature (dragon, moth, etc.)');
 var adjective = prompt('An adjective (beautiful, super, etc.)');
 var verd = prompt('A past tense verb (ran, burped, etc.)');
 
-var a = ('a) Snacking')
-var b = ('b) Escaping')
-var c = ('c) Winning')
+var story = prompt('Which story would you like? \nEnter the letter below: \na) Snacking \nb) Escaping \nc) Winning');
 
-for (i = 0; i < 10; i++) {
-  document.write(letter);
-  document.write('<br>');
+if (story == 'a') {
+  document.write('After snacking on ' + (adjective) + ' treats, the ' + (colour) + ' bellied ' + (creature) + ' ' + (verb) + ' for hours.');
+}
+
+if (story == 'b') {
+  document.write('Ameilia ' + (verb) + ' through the ' + (adjective) + (colour) + ' nebula escaping the space ' + (creature) + '.');
+}
+
+if (story == 'c') {
+  document.write('Jackson chose his ' + (adjective) + ', ' + (colour) + ' ' + (creature) + ' card and ' + (verb) + ' it to the table knowing he won.');
 }
