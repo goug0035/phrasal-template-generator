6839e5e39876283eaef737c732d5b53b4e3476dd
goug0035 <marygough@Marys-MacBook-Pro.local>
2016-02-29 00:01:05 -0500

    var at the top

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
+var colour
+var creature
+var adjective
+var verd
+var story
+
 alert('Let’s make a story together!');
 
-var colour = prompt('A colour (orange, indigo, etc.)');
-var creature = prompt('A creature (dragon, moth, etc.)');
-var adjective = prompt('An adjective (beautiful, super, etc.)');
-var verd = prompt('A past tense verb (ran, burped, etc.)');
+colour = prompt('A colour (orange, indigo, etc.)');
+creature = prompt('A creature (dragon, moth, etc.)');
+adjective = prompt('An adjective (beautiful, super, etc.)');
+verd = prompt('A past tense verb (ran, burped, etc.)');
 
-var story = prompt('Which story would you like? \nEnter the letter below: \na) Snacking \nb) Escaping \nc) Winning');
+story = prompt('Which story would you like? \nEnter the letter below: \na) Snacking \nb) Escaping \nc) Winning');
 
 if (story == 'a') {
   document.write('After snacking on ' + (adjective) + ' treats, the ' + (colour) + ' bellied ' + (creature) + ' ' + (verb) + ' for hours.');
